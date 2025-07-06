0535bbc5277cc4ccd6a2e9226c88901b3c262439
sharmaprinceji <princesh1411@gmail.com>
2025-07-06 17:06:18 +0530

    git repo has been init

---
+++ cmd/delivery-system/main.go
@@ -0,0 +1,9 @@
+package deliverysystem
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Delivery System is running...")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sharmaprinceji/delivery-management-system
+
+go 1.22.2
