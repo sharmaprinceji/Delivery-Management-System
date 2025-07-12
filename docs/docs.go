680148c416f464179d96ed28f092448990fa5a5e
sharmaprinceji <princesh1411@gmail.com>
2025-07-13 02:57:07 +0530

    final done
    Signed-off-by: sharmaprinceji <princesh1411@gmail.com>

--- docs/docs.go
+++ docs/docs.go
@@ -558,7 +558,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "delivery-management-system-h5nh.onrender.com/swagger",
+	Host:             "delivery-management-system-h5nh.onrender.com",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Delivery Management System API",
